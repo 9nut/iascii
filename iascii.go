26c39c7a46fb83522705861c2cf2d18c2a368f47
9Nut <skip.tavakkolian@gmail.com>
2011-02-13 23:59:54 -0800

    files that were overlooked in the initial commit

---
+++ iascii.go
@@ -0,0 +1,34 @@
+//2,|gofmt
+package iascii
+
+import (
+	"image"
+	"io"
+	"os"
+)
+
+func Encode(w io.Writer, m image.Image) (err os.Error) {
+	b := m.Bounds()
+	mw, mh := b.Dx(), b.Dy()
+	if mw <= 0 || mh <= 0 {
+		err = os.NewError("Bad image bounds")
+		return
+	}
+
+	cm := []uint8(".ocOGDQ@")
+	for y := b.Min.Y; y < b.Max.Y; y++ {
+		for x := b.Min.X; x < b.Max.X; x++ {
+			c := image.GrayColorModel.Convert(m.At(x, y)).(image.GrayColor)
+			c.Y >>= 5
+			_, err = w.Write(cm[c.Y : c.Y+1])
+			if err != nil {
+				return
+			}
+		}
+		_, err = w.Write([]uint8("\n"))
+		if err != nil {
+			return
+		}
+	}
+	return
+}
